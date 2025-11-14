8078632098107e086521efe007481737d1735720
hjStack <minnie1498@naver.com>
2025-11-14 16:20:59 +0900

    feat:로그인 페이지에 눈 아이콘 추가[#6]

--- src/main/java/com/example/high_five/controller/TestController.java
+++ src/main/java/com/example/high_five/controller/TestController.java
@@ -6,10 +6,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class TestController {
 
-
     @GetMapping("/")
     public String mainPage() {
 
-        return "main";
+        return "index";
     }
 }
